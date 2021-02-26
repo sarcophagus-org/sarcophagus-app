c30551997b257acc75702272afd39752bcd4a24b
David Colon <dcolonantonio@gmail.com>
2021-02-26 14:05:46 -0500

    changed browserRouter to hashRouter

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,15 +6,14 @@ import Resurrection from './Resurrection'
 import AccuseArchaeologist from './Accuse'
 
 const Body = () => {
-  const { url } = useRouteMatch()
   return (
     <div className="border border-gray-500 p-4 mx-4 mb-8 mt-20 md:mt-0 overflow-x-scroll hide-scrollbar" style={{minHeight: 'calc(100vh - 16rem)'}}>
-      <Route path={`${url}`} exact>
+      <Route path={`/`} exact>
         <Redirect to="/tomb" />
       </Route>
-      <Route path={`${url}tomb`} component={Tomb} />
-      <Route path={`${url}create`} component={CreateSarcophagus} exact />
-      <Route path={`${url}resurrection`} component={Resurrection} exact />
+      <Route path={`/tomb`} component={Tomb} />
+      <Route path={`/create`} component={CreateSarcophagus} exact />
+      <Route path={`/resurrection`} component={Resurrection} exact />
       <Route path="/horus" component={AccuseArchaeologist} exact />
     </div>
   )
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { HashRouter as Router, Switch } from 'react-router-dom'
 import Body from '../components/Body'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
