fd247566e4826b9116fd72a7278072764a31c45b
David Colon <dcolonantonio@gmail.com>
2021-02-26 13:50:10 -0500

    added 404 redirect

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Body from '../components/Body'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 const AppRouter = () => (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <Header />
     <Switch>
       <Body />
