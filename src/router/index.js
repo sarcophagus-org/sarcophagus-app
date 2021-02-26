7ad864b4c0230878241cbbd5f51f14de27d52d0d
David Colon <dcolonantonio@gmail.com>
2021-02-26 12:57:30 -0500

    updated router to handle direct routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Body from '../components/Body'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 const AppRouter = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Header />
     <Switch>
       <Body />
