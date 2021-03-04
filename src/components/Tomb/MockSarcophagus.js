16fa330b21efae1cc08588a8f579379df247ecce
David Colon <dcolonantonio@gmail.com>
2021-03-04 10:44:49 -0500

    shows empty connect button when not connected

--- src/components/Tomb/MockSarcophagus.js
+++ src/components/Tomb/MockSarcophagus.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MockSarcophagus = ({message, handleClick}) => {
     return (
-        <div className="border border-white text-white text-md flex px-4 my-8 pt-2 justify-between relative max-w-128" style={{height: '4.375rem'}}>
+        <div className=" mx-auto border border-white text-white text-md flex px-4 my-8 pt-2 justify-between relative max-w-128" style={{height: '4.375rem'}}>
             <div className="text-center h-full w-full text-white absolute top-0 left-0 flex justify-center items-center cursor-pointer" onClick={handleClick}>{message}</div>
         </div>
     )
--- src/components/create/index.js
+++ src/components/create/index.js
@@ -12,10 +12,14 @@ import SettingsContainer from './SettingsContainer'
 import UploadContainer from './UploadContainer'
 import ResurrectionContainer from './ResurrectionContainer'
 import ArchaeologistContainer from './ArchaeologistContainer'
+import MockSarcophagus from '../Tomb/MockSarcophagus'
+import { connect } from '../../web3/userSupplied'
+import { useWeb3 } from '../../web3'
 
 const CreateSarcophagus = () => {
   const history = useHistory()
   const { archaeologists, createSarcophagus, refresh } = useData()
+  const { account } = useWeb3()
   const { approved, approveTransaction } = useApproval()
   const {file, setFile, handleArchaeologistSelect, handleEmbalming, selectedArchaeologist, handleKey} = useSarcophagusCreate(createSarcophagus)
   const [ buttonText, setButtonText ] = useState('')
@@ -41,14 +45,18 @@ const CreateSarcophagus = () => {
     <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit} validateOnMount >
       {({ values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue, validateForm, isValid }) => (
         <form className="ml-8 px-14" onSubmit={handleSubmit}>
-          <PageHeading />
-          <SettingsContainer values={values} errors={errors} touched={touched} handleChange={handleChange} handleKey={handleKey}/>
-          <UploadContainer values={values} file={file} handleFile={setFile} errors={errors} touched={touched} setFieldValue={setFieldValue} />
-          <ResurrectionContainer values={values} errors={errors} touched={touched} handleChange={handleChange} setFieldValue={setFieldValue}/>
-          <ArchaeologistContainer errors={errors} touched={touched} archaeologists={archaeologists} file={file} values={values} handleChange={handleChange} handleEmbalming={handleEmbalming} handleSelected={handleArchaeologistSelect} archSelected={selectedArchaeologist?.address || ""} setFieldValue={setFieldValue}/>
-          
-          <Button label={buttonText} isDisabled={!isValid} _classnames="my-8" height="lg" type={approved ? 'submit' : 'button'} onClick={approved ? () => validateForm() : () => {validateForm(); handleApproval(errors)}} />
 
+          {!account ? <MockSarcophagus message="Connect to a wallet to get started" handleClick={() => connect()}/> : (
+            <> 
+              <PageHeading />
+              <SettingsContainer values={values} errors={errors} touched={touched} handleChange={handleChange} handleKey={handleKey}/>
+              <UploadContainer values={values} file={file} handleFile={setFile} errors={errors} touched={touched} setFieldValue={setFieldValue} />
+              <ResurrectionContainer values={values} errors={errors} touched={touched} handleChange={handleChange} setFieldValue={setFieldValue}/>
+              <ArchaeologistContainer errors={errors} touched={touched} archaeologists={archaeologists} file={file} values={values} handleChange={handleChange} handleEmbalming={handleEmbalming} handleSelected={handleArchaeologistSelect} archSelected={selectedArchaeologist?.address || ""} setFieldValue={setFieldValue}/>
+          
+              <Button label={buttonText} isDisabled={!isValid} _classnames="my-8" height="lg" type={approved ? 'submit' : 'button'} onClick={approved ? () => validateForm() : () => {validateForm(); handleApproval(errors)}} />
+            </>
+          )}
         </form>
       )}
     </Formik>
