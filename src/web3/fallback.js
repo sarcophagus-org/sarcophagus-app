49d59f0bef85a2dc5204d365ce2d7b9d9f88e9d0
David Colon <dcolonantonio@gmail.com>
2021-03-04 16:16:58 -0500

    updates fallback provider with API keys

--- src/web3/fallback.js
+++ src/web3/fallback.js
@@ -1,14 +1,27 @@
+
 import { useState, useEffect } from 'react'
 import { getDefaultProvider } from 'ethers'
 
-const useFallbackConnect = (next) => {
+const useFallbackConnect = (previousProvider) => {
   const [provider, setProvider] = useState(null)
+  const [defaultProvider] = useState(getDefaultProvider(parseInt(process.env.REACT_APP_DEFAULT_CHAIN_ID, 10), {
+    alchemy: 'bjrgvDbGZBUJ_iZFFd1vIqUWzgrwKWeg',
+    etherscan: 'G61W8715WK65NCMJPFXU8EBQ5VDTT4KCVJ',
+    infura: '800746c8737e4f34bb772ade4532ab83',
+  }))
 
   useEffect(() => {
-    if (provider || !next) return
+    if (previousProvider) {
+      setProvider(null)
+      return
+    }
 
-    setProvider(getDefaultProvider(parseInt(process.env.REACT_APP_DEFAULT_CHAIN_ID, 10)))      
-  }, [provider, next])
+    if (provider) {
+      return
+    }
+
+    setProvider(defaultProvider)
+  }, [provider, previousProvider, defaultProvider])
 
   return provider
 }
