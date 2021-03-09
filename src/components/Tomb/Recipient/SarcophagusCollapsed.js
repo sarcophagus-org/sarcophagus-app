01a6e5050809e09ae4146a95ba569ce3d9bf4a01
David Colon <dcolonantonio@gmail.com>
2021-03-09 10:08:30 -0500

    - update UI to handle long sarcophagus names

--- src/components/Tomb/Recipient/SarcophagusCollapsed.js
+++ src/components/Tomb/Recipient/SarcophagusCollapsed.js
@@ -5,15 +5,25 @@ import StatusBadge from '../../layout/StatusBadge'
 import classnames from 'classnames'
 import ResurrectionTimer from '../ResurrectionTimer'
 import { TIMER_DEFAULT } from '../../../constants'
+import { truncate } from '../../../utils'
+import Tippy from '@tippyjs/react'
 
 const base = "text-white text-md flex justify-between relative cursor-default max-w-128"
 const pointer = "cursor-pointer"
 
-const SarcophagusCollapsed = ({ sarcophagus, error, toggle, status, collapsed, expandedOption=false, timer, color, timers}) => (
+const SarcophagusCollapsed = ({ sarcophagus : { name, state }, error, toggle, status, collapsed, expandedOption=false, timer, color, timers}) => (
     <div className={!expandedOption ? classnames(base) : classnames(base, pointer)} onClick={!expandedOption ? () => null : () => toggle()} style={{height: '4.375rem'}}>
         <div>
-            <div className="text-base font-bold text-white" style={{lineHeight: '1.625rem'}}>{sarcophagus.name}</div>
-            <ResurrectionTimer state={sarcophagus.state} timer={!timers && timer === TIMER_DEFAULT ? 'Unwrapped' : timer} color={timer === TIMER_DEFAULT ? 'text-white' : color} timers={timers}/>
+        {name.length > 30
+                ? (
+                    <Tippy content={name} className="border-2 border-white rounded text-center text-xs font-normal p-2 bg-gray-900 break-words whitespace-pre-wrap">
+                        <div className="text-base font-bold text-white" style={{lineHeight: '1.625rem'}}>{truncate(name, 25, "...", 18)}</div>
+                    </Tippy>
+                )
+                : (
+                    <div className="text-base font-bold text-white" style={{lineHeight: '1.625rem'}}>{name}</div>
+            )}
+            <ResurrectionTimer state={state} timer={!timers && timer === TIMER_DEFAULT ? 'Unwrapped' : timer} color={timer === TIMER_DEFAULT ? 'text-white' : color} timers={timers}/>
         </div>
 
         <div className="flex flex-col">
@@ -24,7 +34,7 @@ const SarcophagusCollapsed = ({ sarcophagus, error, toggle, status, collapsed, e
                     <span>{ error ? "Details" : "Resurrect" }</span>
                 </div>
             )}
-            <StatusBadge status={status} isActive={sarcophagus?.state === 1} isArchived={sarcophagus?.state === 2} error={error} />
+            <StatusBadge status={status} isActive={state === 1} isArchived={state === 2} error={error} />
         </div>
     </div>
 )
